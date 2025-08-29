39c301ba5a4c1ae91cb4be6af14a7018f793504b
Indrawan <indrawanismail0@gmail.com>
2025-08-29 23:37:40 +0800

    feat: better responsive

--- app/components/History.jsx
+++ app/components/History.jsx
@@ -8,11 +8,11 @@ export default function History() {
         <main className="p-4 grid justify-center items-center w-full min-h-screen mt-18 mb-60">
             <div className="grid items-center justify-center mb-12">
                 <h1 className="text-2xl md:text-6xl font-serif text-black text-center mb-8 font-extralight">HMPS SI Highlight</h1>
-                <p className="text-lg md:text-sm text-gray-800 text-center max-w-90 md:max-w-4xl leading-relaxed"
+                <p className="text-lg md:text-sm text-gray-800 text-center max-w-80 md:max-w-4xl leading-relaxed"
                 >Dengan website ini, kalian bisa memantau acara mendatang ataupun acara yang sedang berlangsung dari HMPS SI. Dan juga, kalian dapat mencari informasi mengenai jurusan Sistem Informasi, termasuk informasi tentang peminatan dan lain sebagainya.</p>
             </div>
             <div className="grid md:flex items-center justify-center gap-8 mt-12">
-            <div className="w-90 h-90 md:w-140  md:h-80">
+            <div className="w-80 h-80 md:w-140  md:h-80">
                 <Image className="w-full h-full object-cover rounded-xl"
                 src={img} alt="HMPS SI"/>
             </div>
@@ -26,7 +26,7 @@ export default function History() {
             <div className="grid md:flex items-center justify-center gap-8 mt-32">
             <div className="grid max-w-xl">
             <h1 className="text-2xl md:text-4xl font-serif text-black text-left mb-4 font-extralight">VISI & MISI</h1>
-                <p className="text-sm md:text-sm text-gray-800 text-left max-w-90 md:max-w-xl leading-relaxed"
+                <p className="text-sm md:text-sm text-gray-800 text-left max-w-80 md:max-w-xl leading-relaxed"
                 >Menjadi Himpunan Mahasiswa Sistem Informasi yang unggul dan berdaya saing tinggi, serta mewujudkan rasa kekeluargaan yang kuat, saling mendukung, dan membangun lingkungan yang inklusif bagi semua anggota.</p>
                 <ul className="text-black text-sm mt-2 max-w-3xl mb-2 tracking-wide">
                     <li className="flex items-center justify-start gap-4 mb-2"><CiBookmarkCheck className="w-12 h-12"/>
@@ -46,7 +46,7 @@ export default function History() {
                     </li>
                 </ul>
                 </div>
-            <div className="w-90 h-90 md:w-140  md:h-80">
+            <div className="w-80 h-80 md:w-140  md:h-80">
                 <Image className="w-full h-full object-cover rounded-xl"
                 src={img2} alt="HMPS SI"/>
             </div>
