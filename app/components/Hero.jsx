80de07f0e0545bc48bf291c908c607cb9dcce846
Indrawan <indrawanismail0@gmail.com>
2025-08-31 04:14:48 +0800

    feat: value of humberger menu

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,4 +1,3 @@
-import { cantata } from "../layout";
 import Image from 'next/image';
 import img from './images/hero.jpg'
 import Navbar from "./Navbar";
@@ -10,13 +9,13 @@ export default function Hero() {
             < Navbar/>
             <Image className="absolute inset-0 w-full h-full object-cover"
                 src={img} alt="hmps_uinsi" />
-            <div class="absolute inset-0 bg-gradient-to-b from-white/40 via-white/50 to-white/90"></div>
+            <div className="absolute inset-0 bg-gradient-to-b from-white/40 via-white/50 to-white/90"></div>
 
-            <div class="relative z-10 flex flex-col items-center justify-center min-h-screen px-6">
-                <h1 class={`text-6xl md:text-8xl font-serif text-black text-center mb-8 font-extralight`}>
+            <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-6">
+                <h1 className={`text-6xl md:text-8xl font-serif text-black text-center mb-8 font-extralight`}>
                     Welcome to<br />HMPS SI
                 </h1>
-                <p class="text-lg md:text-xl text-gray-800 text-center max-w-3xl leading-relaxed tracking-wide">
+                <p className="text-lg md:text-xl text-gray-800 text-center max-w-3xl leading-relaxed tracking-wide">
                     On our campus, world-class faculty and talented students come together to
                     create a better world through groundbreaking research, cutting-edge
                     innovations, and transformative scholarly work.
