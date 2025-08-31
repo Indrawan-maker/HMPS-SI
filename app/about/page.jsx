793d917aa5d61714308b9aabc05d3692f1bafda7
Indrawan <indrawanismail0@gmail.com>
2025-09-01 00:51:57 +0800

    feat: hero about done

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,13 +10,13 @@ export default function About() {
         <nav className="">
             <Navbar />
         </nav>
-        <section className="flex items-center justify-center">
-            <div  className="w-280 h-120 bg-slate-700 justify-center rounded-4xl mt-16 relative shadow-2xl shadow-slate-800 flex">
-            <div className="absolute -left-20 bottom-48">
+        <section className="grid md:flex items-center justify-center px-4 md:px-0">
+            <div  className="min-w-46 max-w-280 h-auto bg-slate-700 justify-center rounded-4xl mt-62 md:mt-16 relative shadow-2xl shadow-slate-800 grid md:flex">
+            <div className="absolute md:-left-20 left-1/2 -translate-x-1/2 md:translate-x-0  md:right-0 -top-20 md:top-38">
             <Image className="w-38 h-38 object-contain"
             src={logo} alt="hmps si"/>
             </div>
-            <div className="px-18 py-8">
+            <div className="px-8 md:px-18 py-8 mt-12 md:mt-0 ">
             <h1 className="font-serif text-2xl font-bold mb-4">HMPS SI</h1>
             <p className="mb-4">Himpunan Mahasiswa Sistem Informasi</p>
             <p className="mb-6">HIMA SI adalah organisasi yang terdiri dari mahasiswa program studi Sistem Informasi, dengan tujuan untuk memberikan wadah pengembangan diri melalui banyak kegiatan di dalamnya. Dan juga sebagai perantara seluruh mahasiswa Sistem Informasi untuk menyampaikan aspirasi serta saran mengenai program studi Sistem Informasi. Selain itu, HIMA SI juga berperan sebagai wadah untuk mempererat hubungan antar mahasiswa Sistem Informasi.</p>
