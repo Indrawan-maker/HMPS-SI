eae85f362ff1e9435c5fee2e4487d807ecdb90d9
Indrawan <indrawanismail0@gmail.com>
2025-08-31 13:35:39 +0800

    feat: rouote for news & event

---
+++ app/event/page.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+export default function event() {
+    return (
+        <>
+        <nav>
+            <Navbar />
+        </nav>
+        <main>
+            <h1></h1>
+        </main>
+        </>
+    )
+}
